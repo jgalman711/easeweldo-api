29f80bb963d8564f8fba00022688b13a72462ca4
vagrant <vagrant@homestead>
2023-05-26 07:44:40 +0000

    set default sorting to desc by created_at

--- app/Traits/Filter.php
+++ app/Traits/Filter.php
@@ -7,6 +7,9 @@ use Illuminate\Pagination\LengthAwarePaginator;
 
 trait Filter
 {
+    public const SORT_ASC = 'asc';
+    public const SORT_DESC = 'desc';
+
     protected function applyFilters(Request $request, $query, ?array $searchableColumns = []): LengthAwarePaginator
     {
         if ($request->has('search')) {
@@ -17,12 +20,14 @@ trait Filter
         }
         if ($request->has('sort')) {
             $sortColumn = $request->input('sort');
-            $sortDirection = 'asc';
+            $sortDirection = self::SORT_ASC;
             if (strpos($sortColumn, '-') === 0) {
-                $sortDirection = 'desc';
+                $sortDirection = self::SORT_DESC;
                 $sortColumn = ltrim($sortColumn, '-');
             }
             $query->orderBy($sortColumn, $sortDirection);
+        } else {
+            $query->orderBy('created_at', self::SORT_DESC);
         }
         $perPage = $request->input('per_page', 10);
         return $query->paginate($perPage);
