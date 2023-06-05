5f7e809715829936cf0338b150f35d4fb21c2ef4
vagrant <vagrant@homestead>
2023-06-05 13:55:45 +0000

    settings update fix

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -5,10 +5,22 @@ namespace App\Http\Controllers;
 use App\Http\Requests\SettingRequest;
 use App\Http\Resources\BaseResource;
 use App\Models\Company;
+use App\Models\Setting;
+use App\Services\PeriodService;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class SettingController extends Controller
 {
+    protected $periodService;
+
+    public function __construct(PeriodService $periodService)
+    {
+        $this->periodService = $periodService;
+    }
+
     public function index(Company $company): JsonResponse
     {
         $settings = $company->setting;
@@ -18,11 +30,14 @@ class SettingController extends Controller
     public function update(SettingRequest $request, Company $company): JsonResponse
     {
         $input = $request->validated();
-        $settings = $company->setting;
-        $settings->update($input);
-        return $this->sendResponse(
-            new BaseResource($settings),
-            'Company settings updated successfully. Changes will be applied next period.'
+        $companyPreviousPeriod = $company->periods()->latest()->first();
+        if ($companyPreviousPeriod) {
+            return $this->sendError('Existing period found. Please contact ES administrator.', 409);
+        }
+        $settings = Setting::updateOrCreate(
+            ['company_id' => $company->id],
+            $input
         );
+        return $this->sendResponse(new BaseResource($settings), 'Company settings updated successfully.');
     }
 }
--- app/Http/Requests/SettingRequest.php
+++ app/Http/Requests/SettingRequest.php
@@ -2,16 +2,46 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Period;
+
 class SettingRequest extends BaseRequest
 {
     public function rules(): array
     {
         return [
-            'period_cycle' => 'required|in:weekly,monthly,semi-monthly',
-            'salary_day' => 'required|array',
-            'salary_day.*' => 'integer',
-            'grace_period' => 'required|integer',
-            'minimum_overtime' => 'required|numeric',
+            'period_cycle' => 'required|in:weekly,semi-monthly,monthly',
+            'salary_day' => [
+                'required',
+                'array',
+                function ($attribute, $value, $fail) {
+                    $periodCycle = $this->input('period_cycle');
+                    if ($periodCycle == Period::TYPE_MONTHLY && count($value) !== 1) {
+                        $fail("The $attribute must contain 1 day for monthly period cycle.");
+                    }
+                    if ($periodCycle == Period::TYPE_SEMI_MONTHLY && count($value) !== 2) {
+                        $fail("The $attribute must contain 2 days for semi-monthly period cycle.");
+                    }
+                    if ($periodCycle == Period::TYPE_SEMI_MONTHLY) {
+                        sort($value);
+                        $day1 = \Carbon\Carbon::createFromFormat('d', $value[0]);
+                        $day2 = \Carbon\Carbon::createFromFormat('d', $value[1]);
+
+                        if ($day1->diffInDays($day2) < 15) {
+                            $fail("The salary days for semi-monthly period cycle must be at least 15 days apart.");
+                        }
+                    } elseif ($periodCycle == Period::TYPE_WEEKLY) {
+                        $allowedDays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+                        foreach ($value as $day) {
+                            if (!in_array(strtolower($day), $allowedDays)) {
+                                $fail("Invalid day '$day' found in $attribute.");
+                                break;
+                            }
+                        }
+                    }
+                }
+            ],
+            'grace_period' => 'required|integer|min:0',
+            'minimum_overtime' => 'required|integer|min:0'
         ];
     }
 }
