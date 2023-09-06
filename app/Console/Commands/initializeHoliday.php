13e0bccb3d8306eb6a53995a334700b174dc0cac
vagrant <vagrant@homestead>
2023-09-06 06:45:21 +0000

    fixed holiday console command

--- app/Console/Commands/initializeHoliday.php
+++ app/Console/Commands/initializeHoliday.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Models\Holiday;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class InitializeHoliday extends Command{
@@ -21,21 +23,22 @@ class InitializeHoliday extends Command{
 
         $data = $response->json();
 
-        if ($response->successful()) {
-            $holidays = $data['response']['holidays'];
-
-            foreach ($holidays as $holidayData) {
-                $holiday = new Holiday();
-                $holiday->name = $holidayData['name'];
-                $holiday->description = $holidayData['description'];
-                $holiday->date = $holidayData['date']['iso'];
-                $holiday->type = $holidayData['primary_type'];
-                $holiday->save();
+        DB::transaction(function () use ($data, $response) {
+            if ($response->successful()) {
+                $holidays = $data['response']['holidays'];
+                foreach ($holidays as $holidayData) {
+                    $holiday = new Holiday();
+                    $holiday->name = $holidayData['name'];
+                    $holiday->description = $holidayData['description'];
+                    $holiday->date = Carbon::parse($holidayData['date']['iso'])->format('Y-m-d');
+                    $holiday->type = $holidayData['primary_type'];
+                    $holiday->save();
+                }
+                $this->info('Philippine holidays have been fetched and saved.');
+            } else {
+                $error = $data['error']['message'];
+                $this->error("Failed to fetch holidays. Error: {$error}");
             }
-            $this->info('Philippine holidays have been fetched and saved.');
-        } else {
-            $error = $data['error']['message'];
-            $this->error("Failed to fetch holidays. Error: {$error}");
-        }
+        });
     }
 }
