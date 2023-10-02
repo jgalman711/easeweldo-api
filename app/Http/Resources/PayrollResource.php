5a18c6cbdb3f244d4aa2c73f805081d8ab6eb0cc
vagrant <vagrant@homestead>
2023-10-02 17:41:21 +0000

    fix period null

--- app/Http/Resources/PayrollResource.php
+++ app/Http/Resources/PayrollResource.php
@@ -11,12 +11,16 @@ class PayrollResource extends BaseResource
 
     public function toArray(Request $request): array
     {
-        $start_date = is_string($this->period->start_date)
-            ? new DateTime($this->period->start_date)
-            : $this->period->start_date;
-        $end_date = is_string($this->period->end_date)
-            ? new DateTime($this->period->end_date)
-            : $this->period->end_date;
+        if (isset($this->period) && $this->period->start_date) {
+            $start_date = is_string($this->period->start_date)
+                ? new DateTime($this->period->start_date)
+                : $this->period->start_date;
+            $end_date = is_string($this->period->end_date)
+                ? new DateTime($this->period->end_date)
+                : $this->period->end_date;
+            $data['period_duration'] = $start_date->format('F d') . ' - ' . $end_date->format('F d, Y');
+        }
+        
 
         $data = parent::toArray($request);
         $indeces = ['overtime', 'late', 'absent', 'undertime'];
@@ -25,7 +29,6 @@ class PayrollResource extends BaseResource
             $minutesKey = $index . '_minutes';
             $data[$hoursKey] = isset($data[$minutesKey]) && $data[$minutesKey] ? $data[$minutesKey] / self::SIXTY_MINUTES : null;
         }
-        $data['period_duration'] = $start_date->format('F d') . ' - ' . $end_date->format('F d, Y');
         return $data;
     }
 }
