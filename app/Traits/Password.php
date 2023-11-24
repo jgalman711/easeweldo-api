44f49b00c25456d4380bd4a61ff25274528871d3
vagrant <vagrant@homestead>
2023-11-24 05:08:22 +0000

    refactor password trait

--- app/Traits/Password.php
+++ app/Traits/Password.php
@@ -6,13 +6,12 @@ use Illuminate\Support\Str;
 
 trait Password
 {
-    private const PASSWORD_LENGTH = 6;
-
-    private const EXPIRATION_MINUTES = 60;
-
     public function generateTemporaryPassword(): array
     {
-        return [Str::random(self::PASSWORD_LENGTH), now()->addMinutes(self::EXPIRATION_MINUTES)];
+        return [
+            Str::random(6),
+            now()->addMinutes(60)
+        ];
     }
 
 }
