abb765ce1ddccb5c954f4c7552a7bf1f87a4c90d
vagrant <vagrant@homestead>
2024-01-27 05:35:18 +0000

    format date time in timerecords

--- app/Http/Resources/TimerecordResource.php
+++ app/Http/Resources/TimerecordResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use DateTime;
 use Illuminate\Http\Request;
 
 class TimerecordResource extends BaseResource
@@ -12,16 +13,39 @@ class TimerecordResource extends BaseResource
             "id" => $this->id,
             "company_id" => $this->company_id,
             "employee_id" => $this->employee_id,
-            "clock_in" => $this->clock_in,
-            "clock_out" => $this->clock_out,
-            "expected_clock_in" => $this->expected_clock_in,
-            "expected_clock_out" => $this->expected_clock_out,
-            "original_clock_in" => $this->original_clock_in,
-            "original_clock_out" => $this->original_clock_out,
+            "date" => $this->date(),
+            "clock_in" => $this->timeFormat($this->clock_in),
+            "clock_out" => $this->timeFormat($this->clock_out),
+            "expected_clock_in" => $this->timeFormat($this->expected_clock_in),
+            "expected_clock_out" => $this->timeFormat($this->expected_clock_out),
+            "original_clock_in" => $this->timeFormat($this->original_clock_in),
+            "original_clock_out" => $this->timeFormat($this->original_clock_out),
             "source" => $this->source,
             "remarks" => $this->remarks,
             "attendance_status" => $this->attendance_status,
             "next_action" => $this->next_action
         ];
+    }
+
+    private function timeFormat(?string $dateTime = null): ?string
+    {
+        if ($dateTime) {
+            $datetime = new DateTime($dateTime);
+            return $datetime->format('H:i:s');
+        }
+        return $dateTime;
+    }
+
+    private function date(): ?string
+    {
+        if ($this->clock_in) {
+            $datetimeString = $this->clock_in;
+        } elseif ($this->expected_clock_in) {
+            $datetimeString = $this->expected_clock_in;
+        } else {
+            return null;
+        }
+        $datetime = new DateTime($datetimeString);
+        return $datetime->format('Y-m-d');
     }
 }
