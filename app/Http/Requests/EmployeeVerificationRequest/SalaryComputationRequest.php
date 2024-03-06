2525a3c825ce80703e3dff0f3c9abb2cffd611c2
vagrant <vagrant@homestead>
2024-03-06 19:32:28 +0000

    salary computation request

--- app/Http/Requests/EmployeeVerificationRequest/SalaryComputationRequest.php
+++ app/Http/Requests/EmployeeVerificationRequest/SalaryComputationRequest.php
@@ -3,9 +3,18 @@
 namespace App\Http\Requests\EmployeeVerificationRequest;
 
 use App\Http\Requests\SalaryComputationRequest as BaseSalaryComputationRequest;
+use App\Rules\EarningsValidationRule;
 
 class SalaryComputationRequest extends BaseSalaryComputationRequest
 {
+    public function rules(): array
+    {
+        $rules = parent::rules();
+        $rules['taxable_earnings'] = [new EarningsValidationRule()];
+        $rules['non_taxable_earnings'] = [new EarningsValidationRule()];
+        return $rules;
+    }
+
     public function messages()
     {
         return [
