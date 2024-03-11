25cc35896b2c2afe38dd7044be5d97e8b1ca63d8
vagrant <vagrant@homestead>
2024-03-11 18:03:50 +0000

    nth month disbursement

--- app/Services/Disbursements/AnnualExtraDisbursement.php
+++ app/Services/Disbursements/AnnualExtraDisbursement.php
@@ -2,9 +2,11 @@
 
 namespace App\Services\Disbursements;
 
+use App\Enumerators\PayrollEnumerator;
 use App\Models\Employee;
 use App\Models\Payroll;
 use App\Models\Period;
+use App\Repositories\PayrollRepository;
 use Carbon\Carbon;
 
 class AnnualExtraDisbursement extends BaseDisbursement
@@ -22,8 +24,22 @@ class AnnualExtraDisbursement extends BaseDisbursement
         return Period::create($input);
     }
 
-    public function generatePayroll(Period $period, Employee $employee): Payroll
+    public function generatePayroll(Period $disbursement, Employee $employee): Payroll
     {
-        dd($period);
+        $payrollRepository = app()->make(PayrollRepository::class);
+        $payrolls = $payrollRepository->getEmployeePayrollsByDateRange($employee->id, [
+            'start_date' => $disbursement->start_date,
+            'end_date' => $disbursement->end_date
+        ]);
+
+        $totalAmount = $payrolls->sum('net_income');
+        return Payroll::create([
+            ...$this->input,
+            'employee_id' => $employee->id,
+            'period_id' => $disbursement->id,
+            'status' => PayrollEnumerator::STATUS_TO_PAY,
+            'basic_salary' => $totalAmount / 12,
+            'pay_date' => $this->input['salary_date']
+        ]);
     }
 }
