62b749a2d4aab2015ff2c2a3cc3e30a3b796f9c8
vagrant <vagrant@homestead>
2024-03-11 15:52:24 +0000

    time and attendace calculator for final

--- app/Services/Payroll/GeneratePayrollService.php
+++ app/Services/Payroll/GeneratePayrollService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Payroll;
 
 use App\Enumerators\PayrollEnumerator;
+use App\Exceptions\InvalidPayrollGenerationException;
 use App\Models\Company;
 use App\Models\Employee;
 use App\Models\Leave;
@@ -10,6 +11,7 @@ use App\Models\Payroll;
 use App\Models\Period;
 use App\Repositories\HolidayRepository;
 use App\Services\Contributions\ContributionsService;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\DB;
 
@@ -47,6 +49,7 @@ class GeneratePayrollService
             $this->calculateBasicSalary();
             $this->calculateEarnings();
             $this->calculateHoliday();
+            $this->calculateAttendanceEarnings();
             $this->calculateLeaves();
             $this->calculateContributions();
             $this->payroll->save();
@@ -54,7 +57,7 @@ class GeneratePayrollService
             return $this->payroll;
         } catch (Exception $e) {
             DB::rollBack();
-            throw new Exception($e);
+            throw new InvalidPayrollGenerationException($e);
         }
     }
 
@@ -65,7 +68,7 @@ class GeneratePayrollService
         $this->period = $period;
 
         throw_if($this->period->type == Period::TYPE_SPECIAL,
-            new Exception("Unable to auto-generate disbursement type {$this->period->type}")
+            new InvalidPayrollGenerationException("Unable to auto-generate disbursement type {$this->period->type}")
         );
         $this->salaryComputation = $employee->salaryComputation;
         $this->schedules = $employee->schedules;
@@ -86,13 +89,13 @@ class GeneratePayrollService
         if (!$this->salaryComputation) {
             $this->payroll->status = PayrollEnumerator::STATUS_FAILED;
             $this->payroll->save();
-            throw new Exception(
+            throw new InvalidPayrollGenerationException(
                 "Payroll {$this->payroll->id} generation encountered an error. No salary data found."
             );
         } elseif (!$this->companySettings) {
             $this->payroll->status = PayrollEnumerator::STATUS_FAILED;
             $this->payroll->save();
-            throw new Exception(
+            throw new InvalidPayrollGenerationException(
                 "Payroll {$this->payroll->id} generation encountered an error. No company settings found."
             );
         }
@@ -112,20 +115,72 @@ class GeneratePayrollService
         $this->payroll->non_taxable_earnings = $this->salaryComputation->non_taxable_earnings;
     }
 
+    protected function calculateAttendanceEarnings(): void
+    {
+        $earnings = [];
+        foreach($this->timesheet as $record) {
+            $expectedClockIn = Carbon::parse($record->expected_clock_in);
+            $expectedClockOut = Carbon::parse($record->expected_clock_out);
+            if ($record->clock_in && $record->clock_out) {
+                $clockIn = Carbon::parse($record->clock_in);
+                $clockOut = Carbon::parse($record->clock_out);
+                $date = $expectedClockIn->format('Y-m-d');
+                if ($this->isLate($clockIn, $expectedClockIn)) {
+                    $lateHours = round($clockIn->diffInMinutes($expectedClockIn) / 60, 2);
+                    $earnings[PayrollEnumerator::LATE][] = [
+                        'date' => $date,
+                        'hours' => $lateHours,
+                        'rate' => 1,
+                        'amount' => round($lateHours * $this->salaryComputation->hourly_rate, 2) * -1
+                    ];
+                }
+                if ($clockOut->lt($expectedClockOut)) {
+                    $undertimeHours = round($clockOut->diffInMinutes($expectedClockOut) / 60, 2);
+                    $earnings[PayrollEnumerator::UNDERTIME][] = [
+                        'date' => $date,
+                        'hours' => $undertimeHours,
+                        'rate' => 1,
+                        'amount' => round($undertimeHours * $this->salaryComputation->hourly_rate, 2) * -1
+                    ];
+                }
+                if ($this->isOvertime($clockOut, $expectedClockOut)) {
+                    $rate = $this->salaryComputation->overtime_rate;
+                    $overtimeHours = round($clockOut->diffInMinutes($expectedClockOut) / 60, 2);
+                    $earnings[PayrollEnumerator::OVERTIME][] = [
+                        'date' => $date,
+                        'hours' => $overtimeHours,
+                        'rate' => $rate,
+                        'amount' => round($overtimeHours * $rate * $this->salaryComputation->hourly_rate, 2)
+                    ];
+                }
+            } else {
+                $absentHours = $expectedClockIn->diffInHours($expectedClockOut);
+                $earnings[PayrollEnumerator::ABSENT][] = [
+                    'rate' => 1,
+                    'hours' => $absentHours,
+                    'amount' => round($absentHours * $this->salaryComputation->hourly_rate, 2) * -1
+                ];
+            }
+        }
+        $this->payroll->attendance_earnings = empty($earnings) ? null : $earnings;
+    }
+
     protected function calculateBasicSalary(): void
     {
-        if ($this->period->type == PayrollEnumerator::TYPE_FINAL ||
-            $this->period->type == PayrollEnumerator::TYPE_REGULAR
+        if ($this->salaryComputation->is_clock_required &&
+            $this->period->type == PayrollEnumerator::TYPE_FINAL
         ) {
-            if ($this->salaryComputation->is_clock_required) {
-                $daysCount = $this->timesheet->count();
-                dd($daysCount);
-            } else {
-                $this->payroll->basic_salary = $this->salaryComputation->basic_salary
-                    / self::CYCLE_DIVISOR[$this->period->subtype];
-            }
+            $daysCount = $this->timesheet->count();
+            $this->payroll->basic_salary = $daysCount * $this->salaryComputation->daily_rate;
+        } elseif ($this->period->type == PayrollEnumerator::TYPE_REGULAR ||
+            $this->period->type == PayrollEnumerator::TYPE_FINAL
+        ) {
+            $this->payroll->basic_salary = $this->salaryComputation->basic_salary
+                / self::CYCLE_DIVISOR[$this->period->subtype];
         } else {
-            throw new Exception("Invalid payroll type. Please contact the Easeweldo administrator.");
+            throw new InvalidPayrollGenerationException(
+                "Invalid payroll type. Please contact the Easeweldo administrator."
+            );
         }
     }
 
@@ -216,5 +271,18 @@ class GeneratePayrollService
         $periodId = str_pad($this->period->id, 5, '0', STR_PAD_LEFT);
         $lastNumber = str_pad($this->payroll->id, 7, '0', STR_PAD_LEFT);
         return $companyInitials . date('Ymd') . '-' . $periodId . $employeeId . '-' . $lastNumber;
+    }
+
+    private function isLate(Carbon $clockIn, Carbon $expectedClockIn): bool
+    {
+        return $clockIn->gt($expectedClockIn)
+            && $clockIn->diffInMinutes($expectedClockIn) > $this->companySettings->grace_period;
+    }
+
+    private function isOvertime(Carbon $clockOut, Carbon $expectedClockOut): bool
+    {
+        return $this->companySettings->is_ot_auto_approve
+            && $clockOut->gt($expectedClockOut)
+            && $clockOut->diffInMinutes($expectedClockOut) > $this->companySettings->minimum_overtime;
     }
 }
